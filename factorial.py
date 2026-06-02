d0e92dc1cadebd00be5009a35699277c519f4817
DemonDisguise <rohtrajdan27@gmail.com>
2026-06-02 21:10:30 +0530

    Initial setup

---
+++ factorial.py
@@ -0,0 +1,11 @@
+# Complete this function
+def solve(n: int) -> int:
+    """This function returns factorial of the number
+
+    Args:
+        n (int): number
+
+    Returns:
+        int: factorial of the number
+    """
+    pass
